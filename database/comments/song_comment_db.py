71ba7003ac9a5e6c91d4b6fdd212189f6e4e6856
jinzew <jinzewu0@gmail.com>
2022-12-12 21:20:42 -0500

    change comments -> comment and complete song part

--- database/comments/song_comment_db.py
+++ database/comments/song_comment_db.py
@@ -2,7 +2,7 @@ import pymysql
 import os
 from datetime import datetime
 
-class SongCommentsDB(object):
+class SongCommentDB(object):
 
     def __init__(self):
         pass
@@ -23,15 +23,16 @@ class SongCommentsDB(object):
         return conn
 
     @staticmethod
-    def init_db():
-        conn = SongCommentsDB.get_connection()
-        drop_table_sql = "DROP TABLE IF EXISTS SongComments.comments"
-        drop_db_sql = "DROP DATABASE IF EXISTS SongComments"
-        db_sql = "CREATE DATABASE IF NOT EXISTS SongComments"
-        table_sql = "CREATE TABLE IF NOT EXISTS SongComments.comments(comment_id integer primary key, user_id integer, song_id integer, content varchar(255), date datetime)"  
+    def init_db(reset=False):
+        conn = SongCommentDB.get_connection()
+        drop_table_sql = "DROP TABLE IF EXISTS SongComment.comment"
+        drop_db_sql = "DROP DATABASE IF EXISTS SongComment"
+        db_sql = "CREATE DATABASE IF NOT EXISTS SongComment"
+        table_sql = "CREATE TABLE IF NOT EXISTS SongComment.comment(comment_id integer primary key, user_id integer, song_id integer, content varchar(255), date datetime)"  
         cur = conn.cursor()
-        ret = cur.execute(drop_table_sql)
-        ret = cur.execute(drop_db_sql)
+        if reset:
+            ret = cur.execute(drop_table_sql)
+            ret = cur.execute(drop_db_sql)
         ret = cur.execute(db_sql)
         ret = cur.execute(table_sql)
         return ret
@@ -39,8 +40,8 @@ class SongCommentsDB(object):
     
     @staticmethod
     def get_by_comment_id(id):
-        conn = SongCommentsDB.get_connection()
-        sql = "SELECT * FROM SongComments.comments WHERE comment_id=%s"
+        conn = SongCommentDB.get_connection()
+        sql = "SELECT * FROM SongComment.comment WHERE comment_id=%s"
         cur = conn.cursor()
         cur.execute(sql, args=id)
         res = cur.fetchone()
@@ -57,8 +58,8 @@ class SongCommentsDB(object):
             place_holders.append("%s")
             values.append(v)
         
-        sql = "INSERT INTO SongComments.comments (" + ",".join(cols) + ") VALUES (" + ",".join(place_holders) + ")"
-        conn = SongCommentsDB.get_connection()
+        sql = "INSERT INTO SongComment.comment (" + ",".join(cols) + ") VALUES (" + ",".join(place_holders) + ")"
+        conn = SongCommentDB.get_connection()
         cur = conn.cursor()
         ret = cur.execute(sql, args=values)
 
@@ -66,8 +67,8 @@ class SongCommentsDB(object):
 
     @staticmethod
     def delete_by_comment_id(uid):
-        sql = "DELETE FROM SongComments.comments WHERE comment_id=%s"
-        conn = SongCommentsDB.get_connection()
+        sql = "DELETE FROM SongComment.comment WHERE comment_id=%s"
+        conn = SongCommentDB.get_connection()
         cur = conn.cursor()
         ret = cur.execute(sql, args=uid)
         return ret
@@ -82,8 +83,8 @@ class SongCommentsDB(object):
             values.append(v)
         
         values.append(comment_id)
-        sql = "UPDATE SongComments.comments SET " + ",".join(cols) + " WHERE comment_id=%s"
-        conn = SongCommentsDB.get_connection()
+        sql = "UPDATE SongComment.comment SET " + ",".join(cols) + " WHERE comment_id=%s"
+        conn = SongCommentDB.get_connection()
         cur = conn.cursor()
         ret = cur.execute(sql, args=values)
         return ret
--- database/songs/app.py
+++ database/songs/app.py
@@ -1,48 +1,47 @@
 from flask import Flask, Response, request
-from song_comment_db import SongCommentDB
+from song_db import SongDB
 import json
 
 # Create the Flask application object.
-comment_app = Flask(__name__)
+song_app = Flask(__name__)
 HOST = "localhost"
 PORT = 9001
 
-@comment_app.route("/")
+@song_app.route("/")
 def main():
-    return "Welcome to SongCommentDB"
+    return "Welcome to SongDB"
 
-@comment_app.route("/comment/query/<cid>", methods=["GET"])
-def get_comment_by_id(cid):
-    res = SongCommentDB.get_by_comment_id(cid)
+@song_app.route("/song/query/<cid>", methods=["GET"])
+def get_song_by_id(cid):
+    res = SongDB.get_by_song_id(cid)
     if res:
-        res['date'] = res['date'].strftime('%Y-%m-%d %H:%M:%S')
         return Response(json.dumps(res), status=200, content_type="application/json")
     else:
         return Response("Not Found", status=404, content_type="text/plain")
 
-@comment_app.route("/comment/create", methods=["POST"])
-def create_comment():
+@song_app.route("/song/create", methods=["POST"])
+def create_song():
     content_type = request.headers.get('Content-Type')
     if (content_type != 'application/json'):
         return Response('Content-Type Not Supported', status=400, content_type="text/plain")
     body = request.json
-    res = SongCommentDB.create_comment(body)
+    res = SongDB.create_song(body)
     return Response(json.dumps(res), status=200, content_type="application/json") if res else Response("Fail to create", status=400, content_type="text/plain")
 
-@comment_app.route("/comment/delete/<cid>", methods=["POST"])
-def delete_by_comment_id(cid):
-    res = SongCommentDB.delete_by_comment_id(cid)
+@song_app.route("/song/delete/<cid>", methods=["POST"])
+def delete_by_song_id(cid):
+    res = SongDB.delete_by_song_id(cid)
     return Response(json.dumps(res), status=200, content_type="application/json") if res else Response("Fail to delete", status=400, content_type="text/plain")
     
-@comment_app.route("/comment/update/<cid>", methods=["POST"])
-def update_comment(cid):
+@song_app.route("/song/update/<cid>", methods=["POST"])
+def update_song(cid):
     content_type = request.headers.get('Content-Type')
     if (content_type != 'application/json'):
         return Response('Content-Type Not Supported', status=400, content_type="text/plain")
     body = request.json
-    res = SongCommentDB.update_by_comment_id(cid, body)
+    res = SongDB.update_by_song_id(cid, body)
     return Response(json.dumps(res), status=200, content_type="application/json") if res else Response("Fail to update", status=400, content_type="text/plain")
 
 if __name__ == "__main__":
-    SongCommentDB.init_db(reset=False)
-    comment_app.run(HOST, PORT)
+    SongDB.init_db(reset=False)
+    song_app.run(HOST, PORT)
